e35c6ad661a359964b67c4cd7dbc49ff0fe7b0ce
Aniket Shaw <85289910+aniketshaw29@users.noreply.github.com>
2022-07-26 13:57:45 +0530

    added board & square

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,22 @@
 import React from "react";
+import Board from "./Components/Board";
 
 const App = () => {
-  //a component must return a single elememt
+
   return (
-    //<React.Fargment></React.Fargment>   or   <> </>
-    <>
-      <h1>Welcome to React Aniket Shaw Tic Tac toe App!</h1>
-      <p>Hard to get more minimal than this React app.</p>
-    </>
+    <div>
+      <h1>Tic Tac Toe</h1>
+      <Board/>
+    </div>
   );
 };
 
 export default App;
 
-// export default () => (
+// export default () => (   //a component must return a single elememt
+// //<React.Fargment></React.Fargment>   or   <> </>
 // <>
-//   <h1>Welcome to React Vite Micro App!</h1>
-//   <p>Hard to get more minimal than this React app.</p>
+// <h1>Welcome to React Aniket Shaw Tic Tac toe App!</h1>
+// <p>Hard to get more minimal than this React app.</p>
 // </>
 // );

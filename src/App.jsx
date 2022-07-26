96888610670a88c11302052792e26bc00b651dfa
Aniket Shaw <85289910+aniketshaw29@users.noreply.github.com>
2022-07-26 13:34:54 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const App = () => {
+  //a component must return a single elememt
+  return (
+    //<React.Fargment></React.Fargment>   or   <> </>
+    <>
+      <h1>Welcome to React Aniket Shaw Tic Tac toe App!</h1>
+      <p>Hard to get more minimal than this React app.</p>
+    </>
+  );
+};
+
+export default App;
+
+// export default () => (
+// <>
+//   <h1>Welcome to React Vite Micro App!</h1>
+//   <p>Hard to get more minimal than this React app.</p>
+// </>
+// );
